29b6c26b77d988c2c2116b244b29f33b10e7d530
magmadevnr <magmadev@magmadevnr.sboai.cs.eurecom.fr>
2022-09-21 11:54:15 +0200

    Improved

--- agw_integ_test.sh
+++ agw_integ_test.sh
@@ -14,27 +14,25 @@ echo -e ${GREEN}#########################################${BLACK}
 }
 
 Magma_clone () {
-  echo -e ${GREEN} Clone Magma ${BLACK}
   DIR="$HOME/workspace-1/magma"
   if [ -d "$DIR" ]; then
     echo "Directory Exists ${DIR}"
   else
     mkdir workspace-1 && cd workspace-1
-    git clone $1
+    echo "Give Repo Link"
+    read Repo
+    git clone $Repo
   fi
-
-  exit 1
 
 }
 
 
 Magma_branch () {
-  echo -e ${GREEN}Working on Magma Particular Branch${BLACK}
   cd magma
-  git checkout $2
+  echo "Give the branch name"
+  read Branch
+  git checkout $Branch 
   export MAGMA_ROOT=$HOME/workspace-1/magma
-
-  exit 1
 }
 
 Pre_requisite () {
@@ -44,29 +42,22 @@ echo -e ${GREEN}#########################################${BLACK}
 }
 
 Install_pre_requisites () {
-  echo -e ${GREEN} Install pre requisites ${BLACK}
   sudo curl -O https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
   sudo apt update
   sudo apt install ./vagrant_2.2.19_x86_64.deb
   vagrant plugin install vagrant-vbguest vagrant-disksize vagrant-vbguest vagrant-mutate
   pip3 install --upgrade pip
   pip3 install ansible fabric3 jsonpickle requests PyYAML firebase_admin
-  vagrant plugin install vagrant-vbguest vagrant-disksize vbguest
   echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
   source $HOME/.bashrc
-
-  exit 1
-
 }
 
 Open_network_interfaces () {
-  echo -e ${GREEN} Open up network interfaces for VM ${BLACK}
   sudo mkdir -p /etc/vbox/
   sudo touch /etc/vbox/networks.conf
   sudo sh -c "echo '* 192.168.0.0/16' > /etc/vbox/networks.conf"
   sudo sh -c "echo '* 3001::/64' >> /etc/vbox/networks.conf"
 
-  exit 1
 }
 
 LTE_test () {
@@ -82,8 +73,6 @@ lte_integ_test () {
   export MAGMA_DEV_CPUS=3
   export MAGMA_DEV_MEMORY_MB=9216
   fab integ_test
-
-  exit 1
 
 }
 
